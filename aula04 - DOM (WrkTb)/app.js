3ac16b161763ed2ffc10082ba3a88529131e8c4c
juniorvilas <log_junior@hotmail.com>
2021-08-16 12:17:59 -0300

    aula04

---
+++ aula04 - DOM (WrkTb)/app.js
@@ -0,0 +1,30 @@
+
+//body
+let body = document.querySelector('body');
+console.log(body);
+
+//h1
+
+let h1 = document.querySelector('h1');
+console.log(h1);
+//div
+let div = document.querySelector('div');
+console.log(div);
+
+//section
+
+let section = document.querySelector('section');
+console.log(section);
+
+
+
+let link1 = document.querySelector('#link-1'),
+        link2 = document.querySelector('#link-2'),
+            link3 = document.querySelector('#link-3');
+
+let paragrafos = document.querySelectorAll('.p');
+
+
+// Visualizar no console do navegador Chrome
+console.log(link1, link2, link3, paragrafos);
+
