c3776a4d0b9f9349d4f2b855e3b46b79036f5afe
juniorvilas <log_junior@hotmail.com>
2021-09-01 15:48:32 -0300

    aula 11 coments

--- projetoA/js/app.js
+++ projetoA/js/app.js
@@ -4,39 +4,26 @@ let imagem = ""
 let titulo = ""
 let descricao = ""
 
-/* 
-
-function createCard() {
-    title = document.getElementById("title").value
-    description = document.getElementById("description").value
-    urlImg = document.getElementById("url_img").value
-
-
-    let cardSection = document.getElementById("card");
-    cardSection.innerHTML += `<div> <img src="${urlImg}">
-                                    <h2>${title}</h2>
-                                    <p>${description}<p> </div>`;
-} */
-
-
+//Função criar Card
 function criarCard() {
     
-
+// colando o valor do input dos id selecionados nas variáveis
   let imagem = document.getElementById("input_url").value;
   let titulo = document.getElementById("input_title").value;
   
-
+// inseri no campo selecionado pelo ID uma div com url e texto inseridos no input acima
   let cardSection = document.getElementById("card");
   cardSection.innerHTML += `  <div><img class='grid-item' src="${imagem}" alt="imagem"><p>${titulo}</p></div>`;
 
 }
 
 
-
+// a função pop abre o campo para inserção dos dados no formulário
 function pop() {
   document.getElementById('pop').style.display='block';
 }
 
+// a função fechar fecha o formulário
 function fechar() {
     document.getElementById('pop').style.display='none';
 }
